692248038c85529840055412f4046fd31129bc31
骑兵程序员 <hmjlr123@gmail.com>
2017-04-05 18:45:40 +0800

    open in browser

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,19 @@
 import { window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument } from 'vscode';
 import { Proxy,WebServer } from './server';
+let broswerOpener = require('open');
+let ip = require('ip').address();
 
 export function activate(context: ExtensionContext) {
     console.log('Congratulations, your extension "Bifrost" is now active!');
 
     var server = new WebServer({ port: 80,rootPath:workspace.rootPath });
     server.on();
+    
     commands.registerCommand('extension.bf.open_in_browser', (events) => {
         let filepath = events.fsPath.replace(workspace.rootPath, '').replace(/\\/ig,'/');
-        let url = 'http://10.5.15.99' + (server.options.port == 80 ? '' : ':' + server.options.port) + filepath;
+        console.log(ip);
+        let url = 'http:' + ip + (server.options.port == 80 ? '' : ':' + server.options.port) + filepath;
+        broswerOpener(url);
         console.log(`open:${url}`);
     });
 }
