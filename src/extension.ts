b2519f5ae37f4a555732861af74765aa1f8f93b5
骑兵程序员 <hmjlr123@gmail.com>
2017-04-05 18:37:44 +0800

    context menu

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,14 @@
 import { window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument } from 'vscode';
 import { Proxy,WebServer } from './server';
 
-
 export function activate(context: ExtensionContext) {
     console.log('Congratulations, your extension "Bifrost" is now active!');
 
     var server = new WebServer({ port: 80,rootPath:workspace.rootPath });
     server.on();
+    commands.registerCommand('extension.bf.open_in_browser', (events) => {
+        let filepath = events.fsPath.replace(workspace.rootPath, '').replace(/\\/ig,'/');
+        let url = 'http://10.5.15.99' + (server.options.port == 80 ? '' : ':' + server.options.port) + filepath;
+        console.log(`open:${url}`);
+    });
 }
